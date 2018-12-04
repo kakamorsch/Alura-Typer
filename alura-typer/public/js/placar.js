98a0626a08f9b407942504b4d7dee2569ea4855c
kaio <kakamorsch@gmail.com>
2018-12-04 16:33:26 -0200

    Implementado um banco que armazena o placar do jogador.

--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -1,6 +1,7 @@
 $("#botao-placar").click(function() {
   $(".placar").stop().slideToggle();
 });
+$("#botao-sync").click(sincronizaPlacar);
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Kaio"
@@ -42,4 +43,38 @@ function removeLinha() {
 function mostraPlacar() {
   var placar = $(".placar");
   placar.slideDown();
+}
+function sincronizaPlacar() {
+  var placar = [];
+  var linhas = $("tbody>tr");
+  linhas.each(function () {
+    var usuario = $(this).find("td:nth-child(1)").text();
+    var palavras = $(this).find("td:nth-child(2)").text();
+    
+    var score= {
+      usuario: usuario,
+      pontos: palavras
+    };
+
+    placar.push(score);
+  });
+  var dados = {
+    placar: placar
+  };
+  $.post("http://localhost:3000/placar",dados,function () {
+    console.log("Placar sincronizado com sucesso");
+  });
+}
+
+function atualizaPlacar() {
+  $.get("http://localhost:3000/placar",function(data) {
+
+    $(data).each(function(){
+      var linha= novaLinha(this.usuario, this.pontos);
+      linha.find(".botao-remover").click(removeLinha);
+      $("tbody").append(linha);
+    })
+
+  });
+
 }
